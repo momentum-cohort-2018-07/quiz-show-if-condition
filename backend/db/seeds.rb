7db8db8eff1c4b02cb4f07ca8f0d931ddce8dbc4
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-09-21 12:35:01 -0400

    Did intial prep for seed file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+user = User.new
+user.username = 'admin'
+user.email = 'admin@condition.com'
+user.password = 'admin'
+user.admin = true
+user.save
+
+
+user = User.new
+user.username = 'user'
+user.email = 'user@other.com'
+user.password = 'user'
+user.save
+
+
+CSV.foreach('sample/quizzes.csv', headers: true) do |row|
+  Quiz.create(row.to_h)
+end
+
+CSV.foreach('sample/questions.csv', headers: true) do |row|
+  Question.create(row.to_h)
+end
+
+CSV.foreach('sample/answers.csv', headers: true) do |row|
+  Answer.create(row.to_h)
+end

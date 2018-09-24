23ba9cb46455147e18025947fbed43f0c0026d81
jeanetteobr <jeanette.obr@gmail.com>
2018-09-23 21:32:39 -0400

    Create card component for login and register forms. Add password confirmation to register form. Add style to Card component class.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -12,17 +12,9 @@ class Login extends Component {
       password: '',
       registering: false
     }
-    this.handleUsernameChange = this.handleUsernameChange.bind(this)
-    this.handlePasswordChange = this.handlePasswordChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.register = this.register.bind(this)
-    this.setUser = this.setUser.bind(this)
-  }
-  handleUsernameChange (value) {
-    this.setState({ username: value })
-  }
-  handlePasswordChange (value) {
-    this.setState({ password: value })
+    // this.register = this.register.bind(this)
+    // this.setUser = this.setUser.bind(this)
   }
   setUser (e) {
     e.preventDefault()
@@ -32,9 +24,8 @@ class Login extends Component {
   }
   handleSubmit (e) {
     e.preventDefault()
+    const { username, password } = this.state
     let { setCurrentUser } = this.props
-    let username = this.state.username
-    let password = this.state.password
     apiCalls.login(username, password)
       .then(user => setCurrentUser(user))
   }
@@ -44,18 +35,19 @@ class Login extends Component {
     this.setState({ 'registering': conditional })
   }
   render () {
+    const { username, password } = this.state
     if (this.state.registering) {
-      return (<Register handleSubmit={this.handleSubmit} handleChange={this.handleChange} register={this.register} />
+      return (<Register handleSubmit={this.handleSubmit} handleChange={this.handleChange} setUser={this.setUser} register={this.register} />
       )
     } else {
       return (<div className='column'>
         <a > Login</a>
         <a onClick={e => this.register(e, true)}> Register</a>
         <Label>Username</Label>
-        <Input className='username' onChange={event => this.handleUsernameChange(event.target.value)} />
+        <Input className='username' value={username} onChange={(e) => this.setState({username: e.target.value})} />
         <Label>Password</Label>
-        <Input className='username' type='password' onChange={event => this.handlePasswordChange(event.target.value)} />
-        <Button className='is-primary' type='submit' onClick={e => this.setUser(e)}>Submit</Button>
+        <Input className='username' value={password} type='password' onChange={e => this.setState({password: e.target.value})} />
+        <Button className='is-primary' type='submit' onClick={(e) => this.setUser(e)}>Submit</Button>
       </div>
       )
     }

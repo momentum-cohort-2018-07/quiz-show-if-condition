57a34f7cdba7bd9f6fd5cfd7176f7b5eba70fb3a
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-09-24 14:29:39 -0400

    Improved code and fixed typo

--- backend/app/controllers/api/scores_controller.rb
+++ backend/app/controllers/api/scores_controller.rb
@@ -3,7 +3,7 @@ class API::ScoresController < ApplicationController
     @quiz = Quiz.find(params[:quiz_id])
     @responses = Response.where({user_id: current_user.id, quiz_id: @quiz.id})
     @number_correct = @responses.sum('CASE WHEN "responses"."correct" THEN 1 ELSE 0 END')
-    if @response.count != @quiz.questions.count
+    if @responses.count != @quiz.questions.count
       render json: {error: "Cannot score an unfinished quiz"}, status: :unauthorized
     elsif !@quiz.published
       render json: {error: "Cannot score an unpublished quiz"}, status: :unauthorized
